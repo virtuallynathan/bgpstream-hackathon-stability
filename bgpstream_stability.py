b27c6de0485b481bef9b9c51a850782e526cf35f
Nathan Owens <Nathan_Owens@cable.comcast.com>
2016-02-06 10:56:15 -0800

    add per-prefix counters

--- bgpstream_stability.py
+++ bgpstream_stability.py
@@ -20,11 +20,9 @@
 #
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
 from collections import defaultdict
-
 
 updates = defaultdict(lambda : defaultdict(int))
 ​
