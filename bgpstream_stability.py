1d869d4a797a9c74ec23b88051767741ab0ceab6
Nathan Owens <Nathan_Owens@cable.comcast.com>
2016-02-07 10:29:32 -0800

    changes

--- bgpstream_stability.py
+++ bgpstream_stability.py
@@ -82,7 +82,6 @@ def get_ripe_probes(prefix_list):
 		bucket_data = prefix_data[2]
 
         print prefix, count, bucket_data
-
 		url = "https://atlas.ripe.net/api/v1/probe/?format=json&prefix_%s=%s" %(ip_proto, prefix)
 		probe_data = requests.get(url).json()
 
