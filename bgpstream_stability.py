56ef22b8568f53db74f797f12e4027da6475090f
Andrew Mulhern <andrew_mulhern@cable.comcast.com>
2016-02-07 22:33:13 +0000

    Fixing everything

--- bgpstream_stability.py
+++ bgpstream_stability.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 #
-# This file is part of pybgpstream
-#
-# CAIDA, UC San Diego
-# bgpstream-info@caida.org
-#
-# Copyright (C) 2015 The Regents of the University of California.
-# Authors: Alistair King, Chiara Orsini
+# Copyright (C) 2015
+# Authors: Nathan Owens & Andrew Mulhern
 #
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -22,33 +17,30 @@
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import pdb
+
+import sys
 import json
 import copy
 import math
-import requests
-#import multiprocessing
+import urllib
+import multiprocessing
 from _pybgpstream import BGPStream, BGPRecord, BGPElem
 from collections import defaultdict
-#from netaddr import IPNetwork, IPAddress
 from datetime import datetime
-from ripe.atlas.cousteau import (
-    Ping,
-    Traceroute,
-    AtlasSource,
-    AtlasCreateRequest
-)
+
 
 def deal_with_time_bucket_junk(prefix, timestamp):
 
-    if prefix not in prefixData:
+    if prefix not in raw_bgp_stream_data:
         newBuckets = copy.deepcopy(buckets)
-        prefixData[prefix] = newBuckets
+        raw_bgp_stream_data[prefix] = newBuckets
 
     duration = timestamp - stream_start
     bucket = int(duration / 300)
-    # pick correct bucket -> then
-    prefixData[prefix][bucket]["count"] += 1
+    try:
+        raw_bgp_stream_data[prefix][bucket]["count"] += 1
+    except:
+        pass
 
 
 def create_time_buckets(start, end):
@@ -68,18 +60,11 @@ def get_ripe_probes(prefix_list):
         prefix = prefix_data[0]
         count = prefix_data[1]
         bucket_data = prefix_data[2]
-        print prefix
 
         url = "https://atlas.ripe.net/api/v1/probe/?format=json&prefix_%s=%s" % (ip_proto, prefix)
-        print url
-        pdb.set_trace()
-        print "ajm - 1"
-        probe_data = requests.get(url)
-        print "ajm - 2"
+        probe_data = json.loads(urllib.urlopen(url).read())
 
         probe_count = probe_data["meta"]["total_count"]
-        print "ajm - 3"
-        print url, probe_count
 
         probe_ids = []
         if probe_count > 0:
@@ -87,149 +72,116 @@ def get_ripe_probes(prefix_list):
                 probe_id = probe["id"]
                 probe_ids.append(probe_id)
 
-        return_dict[prefix] = {"count": count, "bucket_data": bucket_data, "probe_count": probe_count, "probe_ids": probe_ids}
+	if len(probe_ids) > 0:
+		return_dict[prefix] = {"count": count, "bucket_data": bucket_data, "probe_count": probe_count, "probe_ids": probe_ids}
+	return
 
     jobs = []
-    #manager = multiprocessing.Manager()
-    #return_dict = manager.dict()
-    return_dict = {}
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
 
     for prefix_data in prefix_list:
         prefix = prefix_data[0]
 
         if "." in prefix:
-            get_probe_list("v4", prefix_data, return_dict)
-        #    job = multiprocessing.Process(target=get_probe_list, args=("v4", prefix_data, return_dict))
+            job = multiprocessing.Process(target=get_probe_list, args=("v4", prefix_data, return_dict))
 
         elif ":" in prefix:
-            get_probe_list("v6", prefix_data, return_dict)
+            job = multiprocessing.Process(target=get_probe_list, args=("v6", prefix_data, return_dict))
 
-        #    job = multiprocessing.Process(target=get_probe_list, args=("v6", prefix_data, return_dict))
+        jobs.append(job)
+        job.start()
 
-        #jobs.append(job)
-        #job.start()
-
-    #for job in jobs:
-    #    job.join()
+    for job in jobs:
+        job.join()
 
     return dict(return_dict)
 
 
-def create_ripe_measurement(prefix_list):
-    measurement_count = 0
-    measurement_limit = 10
-    ATLAS_API_KEY = "secret"
-    for prefix, ip_list in prefix_list.iteritems():
-        for ip in ip_list:
+if __name__ == "__main__":
 
-            ipAddr = ip
-            count = 0
-            descr = "Prefix: " + prefix + "Flapped " + str(count) + " times"
+    try:
+        stream_start = int(sys.argv[1])
+        stream_end = int(sys.argv[2])
+        out_file_name = sys.argv[3]
 
-            ping = Ping(af=4, target=ipAddr, description=descr)
+    except:
+        print "Usage: %s [start time] [end time] [output file name]" %(sys.argv[0])
+        exit()
 
-            traceroute = Traceroute(
-                af=4,
-                target=ipAddr,
-                description=descr,
-                protocol="ICMP",
-            )
-
-            source = AtlasSource(type="area", value="WW", requested=5)
-
-            atlas_request = AtlasCreateRequest(
-                start_time=datetime.utcnow(),
-                key=ATLAS_API_KEY,
-                measurements=[ping, traceroute],
-                sources=[source],
-                is_oneoff=True
-            )
-
-            (is_success, response) = atlas_request.create()
-            measurement_count += 1
-            if measurement_count > measurement_limit:
-                break
-
-
-updates = defaultdict(int)
-prefixData = {}
-# create a new bgpstream instance
-stream = BGPStream()
-
-# create a reusable bgprecord instance
-rec = BGPRecord()
-
-# configure the stream to retrieve Updates records from the RRC06 collector
-stream.add_filter('collector', 'rrc06')
-stream.add_filter('record-type', 'updates')
-
-stream_start = 1454284800
-stream_end = 1454285700
-# select the time interval to process:
-# Wed Apr 1 00:02:50 UTC 2015 -> Wed Apr 1 00:04:30
-stream.add_interval_filter(stream_start, stream_end)
-buckets = create_time_buckets(stream_start, stream_end)
-
-# start the stream
-stream.start()
-
-while(stream.get_next_record(rec)):
-
-    elem = rec.get_next_elem()
-
-    while(elem):
-
-        communities = elem.fields.get("communities", "")
-        nextHop = elem.fields.get("next-hop", "")
-        prefix = elem.fields.get("prefix", "")
-        asPath = elem.fields.get("as-path", "")
-        asPathList = asPath.split(' ')
-        time_stamp = rec.time  # unix epoc timestamp 1427846670
-
-        currCount = updates.get(prefix)
-        deal_with_time_bucket_junk(prefix, time_stamp)
-
-        if not currCount:
-            currCount = 0
-
-        currCount += 1
-        updates[prefix] = currCount
-
+    #stream_start = 1454284800
+    #stream_end = 1454288400
+    buckets = create_time_buckets(stream_start, stream_end)
+    
+    prefixList = []
+    raw_bgp_stream_data = {}
+    
+    stream = BGPStream()
+    rec = BGPRecord()
+    stream.add_filter('collector', 'rrc06')
+    stream.add_filter('record-type', 'updates')
+    stream.add_interval_filter(stream_start, stream_end)
+    stream.start()
+    
+    while(stream.get_next_record(rec)):
+    
         elem = rec.get_next_elem()
+    
+        while(elem):
+    
+            prefix = elem.fields.get("prefix", "")
+            time_stamp = rec.time  # unix epoc timestamp 1427846670
+    
+            if prefix != "":
+    		deal_with_time_bucket_junk(prefix, time_stamp)
+    
+            elem = rec.get_next_elem()
+    
+    
+    for prefix in list(raw_bgp_stream_data):
+        for bucket in list(raw_bgp_stream_data[prefix]):
+            if bucket["count"] < 3:
+                raw_bgp_stream_data[prefix].remove(bucket)
+    
+    
+    for prefix in raw_bgp_stream_data:
+    
+        index = 0
+        max_index = 0
+        max_val = 0
+        last_val = 0
+    
+        for bucket in raw_bgp_stream_data[prefix]:
+    
+            curr = bucket["count"]
+            if curr > last_val:
+                max_val = curr
+    
+            index += 1
+            last_val = curr
+    
+        if raw_bgp_stream_data[prefix]:
+            prefixList.append((prefix, max_val, raw_bgp_stream_data[prefix][max_index]))
+    
+    
+    prefixListWithProbes = get_ripe_probes(prefixList)
+    
+    
+    import get_probe_data
+    results = []
+    
+    for prefix, values in prefixListWithProbes.iteritems():
+        start_time = values["bucket_data"]["start"]
+        end_time = values["bucket_data"]["end"]
+        count = values["count"]
+    
+        probe_list = values["probe_ids"]
+        for probe in probe_list:
+            packet_loss = get_probe_data.get_packet_loss(probe, start_time, end_time)
 
-
-
-for prefix in list(prefixData):
-    for bucket in list(prefixData[prefix]):
-        if bucket["count"] < 3:
-            prefixData[prefix].remove(bucket)
-
-prefixList = []
-
-for prefix in prefixData:
-
-    index = 0
-    max_index = 0
-    max_val = 0
-    last_val = 0
-
-    for bucket in prefixData[prefix]:
-
-        curr = bucket["count"]
-        if curr > last_val:
-            max_val = curr
-
-        index += 1
-        last_val = curr
-
-    if prefixData[prefix]:
-        prefixList.append((prefix, max_val, prefixData[prefix][max_index]))
-
-
-sortedPrefixList = sorted(prefixList, key=lambda x: (x[1], x[0]))
-lastNPrefixList = sortedPrefixList[-20:]
-
-#print json.dumps(lastNPrefixList, indent=4)
-print json.dumps(get_ripe_probes(lastNPrefixList), indent=4)
-
-#print json.dumps(ripeProbeList, indent=4)
+    	results.append({"prefix":prefix, "count":count, "start_time":start_time, "end_time":end_time, "probe":probe, "packet_loss":packet_loss})
+    
+    
+    out_file = open(out_file_name, "w")
+    out_file.write(json.dumps(results, indent=3))
+    out_file.close()
